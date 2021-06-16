6c7422a0e88396b2c53a8f9986e70c4d156d93ab
kjs0619-prog <84962654+kjs0619-prog@users.noreply.github.com>
2021-06-16 15:43:09 +0900

    20210616 수정커밋했습니다. 확인부탁드립니다.

--- myjavaproject/src/myjavaproject/first/MyJavaProject.java
+++ myjavaproject/src/myjavaproject/first/MyJavaProject.java
@@ -5,7 +5,8 @@ public class MyJavaProject {
 	public static void main(String[] args) {
 
 //		System.out.println("æ·≥Á«œººø‰");
-		System.out.println("æ·≥Á«œººø‰2");
+//		System.out.println("æ·≥Á«œººø‰2");
+		System.out.println("¡¶¿Ã∏ß¿∫ 000¿‘¥œ¥Ÿ.");
 	}
 
 }
