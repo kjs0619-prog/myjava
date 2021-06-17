bf7ef53210ae7e8f75d55428c2d0d42cbdab6433
kjs0619-prog <84962654+kjs0619-prog@users.noreply.github.com>
2021-06-17 11:33:28 +0900

    커밋 해보자

--- myjavaproject/src/myjavaproject/first/MyJavaProject.java
+++ myjavaproject/src/myjavaproject/first/MyJavaProject.java
@@ -1,13 +1,14 @@
 package myjavaproject.first;
 /**
  * https://docs.oracle.com/en/java/javase/16/
- * @author kjs
+ * @author kjs 
  * Note: This is my project. 
  */
 public class MyJavaProject {
 
-	/** my description Only English?*/
+	/** my description Only English? 아니면 한글 출력 가능 */
 	public static void main(String[] args) {
+		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		
