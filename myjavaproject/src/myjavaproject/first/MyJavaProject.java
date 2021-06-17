518c054e321c4095873f1f18e4db21e5461c7916
kjs0619-prog <84962654+kjs0619-prog@users.noreply.github.com>
2021-06-17 14:14:05 +0900

    요약
    커밋하기

--- myjavaproject/src/myjavaproject/first/MyJavaProject.java
+++ myjavaproject/src/myjavaproject/first/MyJavaProject.java
@@ -6,12 +6,15 @@ package myjavaproject.first;
  */
 public class MyJavaProject {
 
-	/** my description Only English? 아니면 한글 출력 가능 */
+	/** my description Only English? 아니면 한글 출력 가능 
+	 * javadoc 만드는 자세한 설명은 tutorial을 참고하시기 바랍니다. 
+	 * @parm any string array
+	 * */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
-		
+		//테스트 중임 
 	}
 
 }
