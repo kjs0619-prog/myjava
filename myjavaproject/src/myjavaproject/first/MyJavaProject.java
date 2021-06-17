19628c5bf39c6dfeab688fb3f5553884aeba9a63
kjs0619-prog <84962654+kjs0619-prog@users.noreply.github.com>
2021-06-17 10:47:27 +0900

    오늘 수정 본 커밋

--- myjavaproject/src/myjavaproject/first/MyJavaProject.java
+++ myjavaproject/src/myjavaproject/first/MyJavaProject.java
@@ -1,12 +1,14 @@
 package myjavaproject.first;
-
+/**
+ * https://docs.oracle.com/en/java/javase/16/
+ * @author kjs
+ * Note: This is my project. 
+ */
 public class MyJavaProject {
 
+	/** my description Only English?*/
 	public static void main(String[] args) {
-
-//		System.out.println("얀녕하세요");
-//		System.out.println("얀녕하세요2");
-		System.out.println("제이름은 000입니다");
+		System.out.println("Hello World!");
 	}
 
 }
